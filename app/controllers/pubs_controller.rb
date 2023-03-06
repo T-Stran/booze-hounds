932b33a0b973d83bd83345bb75abd15a9f29a419
Tom Stranack <tomstranack@yahoo.co.uk>
2023-03-06 16:18:40 +0000

    added the map to index

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -1,2 +1,13 @@
 class PubsController < ApplicationController
+
+  def index
+    @pubs = Pub.all
+    # The `geocoded` scope filters only flats with coordinates
+    @markers = @pubs.geocoded.map do |pub|
+      {
+        lat: pub.latitude,
+        lng: pub.longitude
+      }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :locals
   end
 
+  resources :pub
   resources :dogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
