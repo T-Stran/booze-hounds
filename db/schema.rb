2dc36cab095de41fbabb4cc37789464d69e76a57
Tom Stranack <tomstranack@yahoo.co.uk>
2023-03-06 14:58:00 +0000

    homepage basic

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,59 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_121913) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_06_141544) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "breed"
+    t.string "proffession"
+    t.integer "age"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_dogs_on_user_id"
+  end
+
+  create_table "locals", force: :cascade do |t|
+    t.bigint "pub_id", null: false
+    t.bigint "dog_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_locals_on_dog_id"
+    t.index ["pub_id"], name: "index_locals_on_pub_id"
+  end
+
+  create_table "pubs", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.date "opening_time"
+    t.date "closing_time"
+    t.string "website"
+    t.integer "phone_number"
+    t.boolean "pool_table"
+    t.boolean "non_alcoholic_drinks_selection"
+    t.boolean "garden"
+    t.boolean "parking"
+    t.boolean "live_sport"
+    t.boolean "wheelchair_accessible"
+    t.boolean "food_menu"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "pub_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pub_id"], name: "index_reviews_on_pub_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +76,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_121913) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "dogs", "users"
+  add_foreign_key "locals", "dogs"
+  add_foreign_key "locals", "pubs"
+  add_foreign_key "reviews", "pubs"
+  add_foreign_key "reviews", "users"
 end
