851a2f971e078fbe520b939fe7a4c3e7f05b8894
James Corfe <james.corfe@gmail.com>
2023-03-06 16:42:49 +0000

    controllers and views

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,47 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+  def new
+    @dog = Dog.new(params[:id])
+  end
+
+  def create
+    @dog = Dog.new(dog_params)
+    @dog.user = current_user
+    if @dog.save
+      redirect_to dog_path(@dog)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @dog = Dog.find(params[:id])
+  end
+
+  def update
+    @dog = Dog.find(params[:id])
+    @dog.update(dog_params)
+    redirect_to dogs_path(@dog)
+  end
+
+  def destroy
+    @dog = Dog.find(params[:id])
+    @dog.destroy
+    redirect_to dogs_path, status: :see_other
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :profession, :age, :user_id)
+  end
+
 end
--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -37,7 +37,7 @@ class PubsController < ApplicationController
   end
 
   def destroy
-    @pub = Pub.ind(params[:id])
+    @pub = Pub.find(params[:id])
     @pub.destroy
     redirect_to pubs_path, status: :see_other
   end
@@ -45,6 +45,8 @@ class PubsController < ApplicationController
   private
 
   def pub_params
-    params.require(:pub).permit(:name, :description, :armaments, :capacity, :price, :speed, :photo) #TO BE UPDATED WITH CORRECT PARAMS
+    params.require(:pub).permit(:name, :address, :description, :opening_time, :closing_time, :website, :phone_number,
+                                :pool_table, :non_alcoholic_drinks_selection, :garden, :parking, :live_sport,
+                                :wheelchair_accessible, :food_menu)
   end
 end
