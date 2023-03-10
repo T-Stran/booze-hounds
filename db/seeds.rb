3ebd2c50df819fdcd533518a7e73fe668d4933c4
Tom Stranack <tomstranack@yahoo.co.uk>
2023-03-10 16:53:49 +0000

    fixed seed again

--- db/seeds.rb
+++ db/seeds.rb
@@ -62,6 +62,7 @@ pub_description = ["Fashionably updated Victorian pub with wood floors, big wind
                    "Victorian pub namechecked in the rhyme Pop Goes the Weasel, with courtyard garden and weekly quiz."]
 
 puts "Destroying everything"
+RoomMessage.destroy_all
 Dog.destroy_all
 User.destroy_all
 Pub.destroy_all
@@ -78,7 +79,7 @@ urls.each do |url_select|
   response = http.request(request)
   obj = JSON.parse(response.read_body)
 
-  
+
   obj["data"].each do |line|
     pub = Pub.new(name: line["name"], address: line["address_obj"]["street1"], postcode: line["address_obj"]["postalcode"], description: pub_description.sample, opening_time: opening.sample, closing_time: closing.sample, phone_number: line["location_id"], pool_table: [true, false].sample, non_alcoholic_drinks_selection: [true, false].sample, garden: [true, false].sample, parking: [true, false].sample, live_sport: [true, false].sample, wheelchair_accessible: [true, false].sample, food_menu: [true, false].sample)
     file = URI.open(pub_photos.sample)
