559f42b87b46d242d9a907e178965fe5dea48202
jerome junkeereeket <jjunkeereeket11@gmail.com>
2023-03-14 11:41:45 +0000

    a lot of chnages were made BIG CHECK NEEEDED

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_13_143241) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_143434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,18 +65,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_13_143241) do
     t.bigint "pub_id", null: false
     t.index ["pub_id"], name: "index_favourites_on_pub_id"
     t.index ["user_id"], name: "index_favourites_on_user_id"
-  end
-
-  create_table "installs", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_installs_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_installs_on_reset_password_token", unique: true
   end
 
   create_table "locals", force: :cascade do |t|
