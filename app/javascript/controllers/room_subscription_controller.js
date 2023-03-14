3310d0c555ec440443c4924d2281c54c6ffa40ba
jerome junkeereeket <jjunkeereeket11@gmail.com>
2023-03-14 10:38:39 +0000

    fixed connect method inserting it in the export default in the soom_subscription js file

--- app/javascript/controllers/room_subscription_controller.js
+++ app/javascript/controllers/room_subscription_controller.js
@@ -6,19 +6,11 @@ export default class extends Controller {
   static values = { roomId: Number }
   static targets = ["room_messages"]
 
-
-
-
   connect() {
-    console.log(`Subscribe to the room with the id ${this.chatroomIdValue}.`)
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "RoomChannel", id: this.roomIdValue },
+      { received: data => console.log(data) }
+    )
+    console.log(`Subscribed to the room with the id ${this.roomIdValue}.`)
   }
-}
-
-
-connect() {
-  this.channel = createConsumer().subscriptions.create(
-    { channel: "RoomChannel", id: this.roomIdValue },
-    { received: data => console.log(data) }
-  )
-  console.log(`Subscribed to the room with the id ${this.roomIdValue}.`)
 }
